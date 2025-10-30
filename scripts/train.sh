4fc9c0c589fdc31a8c305336aca53b0fabc01d6e
Natnael Taye Tsegaye <158073347+natnaeltaye@users.noreply.github.com>
2025-10-30 13:54:31 -0400

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,9 +5,9 @@ source ~/yolovenv/bin/activate
 
 # Train the YOLOv11 segmentation model
 yolo task=segment mode=train \
-  model=yolo11x-seg.pt \
+  model=yolo11l-seg.pt \
   data=/home/natnael/refined-concrete-defect-dataset-06142025/data.yaml \
-  epochs=300 imgsz=640 batch=80 \
+  epochs=300 imgsz=640 batch=115 \
   lr0=0.001 lrf=0.01 momentum=0.937 weight_decay=0.0005 \
   patience=30 augment=True save=True warmup_epochs=3 \
   degrees=30 translate=0.1 scale=0.5 shear=2 perspective=0.0005 \
